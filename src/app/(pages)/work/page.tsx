7017eca536a6c3d5ad694530650ac2ddbd4b292c
AJBatanes11 <anthonybatanes@gmail.com>
2025-05-21 20:56:17 +0800

    &apos;

--- src/app/(pages)/work/page.tsx
+++ src/app/(pages)/work/page.tsx
@@ -8,14 +8,14 @@ export default function Work() {
         <div className="container w-11/12 md:w-4/5 max-w-7xl mx-auto min-h-96 pt-20 sm:pt-0 relative flex items-end">
           <div className="xl:absolute xl:-bottom-10 left-0 xl:max-w-[41rem] 2xl:max-w-[45rem]">
             <h2 className="text-4xl mb-5">
-              I've worked with leading brands to ambitious start-ups deliver
-              ecommerce websites that that are built to perform.
+              I&apos;ve worked with leading brands to ambitious start-ups
+              deliver ecommerce websites that that are built to perform.
             </h2>
             <p>
               As a, Shopify specialist, ecommerce web design agency, we combine
               our technical and creative skills to create ecommerce websites
-              that engage your customers to keep them coming back. Don't take
-              our word for it.
+              that engage your customers to keep them coming back. Don&apos;t
+              take our word for it.
             </p>
           </div>
         </div>
