c7ec41ad9cf55e7e91a95abaa231b88501ec9927
AJBatanes11 <anthonybatanes@gmail.com>
2025-06-15 16:36:38 +0800

    Update aboutHero2.tsx

--- src/app/components/aboutHero2.tsx
+++ src/app/components/aboutHero2.tsx
@@ -62,7 +62,7 @@ export default function AboutHero() {
         {/* Text Overlay */}
         <div className="absolute inset-0 flex items-center justify-center">
           <h1 className="zoom-text text-white mix-blend-difference text-4xl sm:text-5xl md:text-6xl font-bold text-center px-4">
-            About AJ Batanes
+            About me
           </h1>
         </div>
       </div>
